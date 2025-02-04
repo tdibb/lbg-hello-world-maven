c04d977b026a31e6d72e3c5c2e62d8d5736cac30
Trudy <trudy@gmail.com>
2025-02-04 14:21:08 +0000

    changed names

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -9,10 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println(sayHello());
-        System.out.println(sayHelloToSomeone("Alice"));
+        System.out.println(sayHelloToSomeone("Ralf"));
+        System.out.println(sayHelloToSomeone("Bernard"));
         System.out.println(sayHelloToSomeone("Bob"));
-        System.out.println(sayHelloToSomeone("Charlie"));
-        System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("Trudy"));
         System.out.println(sayGoodbye());
     }
 
